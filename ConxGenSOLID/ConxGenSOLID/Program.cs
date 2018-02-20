68c3aefddb486110b92027c0487e2c7654660dc2
ecaracasg <34006770+elnnoncaracas@users.noreply.github.com>
2018-02-19 19:33:28 -0600

    Pruebas de Conexion
    Se realizaron Pruebas en SQL Server y PostgresSQL - Ambas fueron Exitosas

--- ConxGenSOLID/ConxGenSOLID/Program.cs
+++ ConxGenSOLID/ConxGenSOLID/Program.cs
@@ -16,7 +16,8 @@ namespace ConxGenSOLID
             //ConInit.Despelote();
             InitConexion Conextad = new InitConexion();
             ConCloser Cierra = new ConCloser();
-            Conextad.ConectarDB(@"Data Source=DESKTOP-8MI8C4P;Initial Catalog=EMPRESA;Integrated Security=True", "System.Data.SqlClient");
+            //Conextad.ConectarDB(@"Data Source=DESKTOP-8MI8C4P;Initial Catalog=EMPRESA;Integrated Security=True", "System.Data.SqlClient");/*MS-SQL SERVER*/
+            //Conextad.ConectarDB(@"Server =babar.elephantsql.com; Port = 5432; Database = bmfkgevd; User Id = bmfkgevd; Password =  1erS7WgQ83Ql3i9DRjJk5mT_CV64jTwX", "Npgsql");/*POSTGRES SQL*/
             Console.Read();
             ConCommander mande = new ConCommander();
             mande.consultaR(@"Select * from EMPLEADO");
